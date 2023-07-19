fbb22c4e1debe29e2dfd6a5c830237ba9a0f0d50
DarkElf2233 <fd.melnikoff@gmail.com>
2023-07-19 18:39:35 +0300

    Add exception for main tag

--- articles/admin.py
+++ articles/admin.py
@@ -1,11 +1,29 @@
 
 from django.contrib import admin
+from django.forms import BaseInlineFormSet
+from django.core.exceptions import ValidationError
 from .models import Article, Tags, ArticleTagsPositions
+
+
+class PositionsInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        count_ismain = 0
+        for form in self.forms:
+            if form.cleaned_data.get('is_main', False):
+                count_ismain += 1
+            if count_ismain > 1:
+                raise ValidationError('Основной тег может быть только один')
+
+        if count_ismain == 0:
+            raise ValidationError('Укажите основной тег')
+
+        return super().clean()
 
 
 class ArticleTagsPositionsInline(admin.TabularInline):
     model = ArticleTagsPositions
     extra = 3
+    formset = PositionsInlineFormset
 
 
 @admin.register(Article)
